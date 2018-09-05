55e5286ed3f7655377150b84f60349537bd99c53
Robby <ta7382@gmail.com>
2018-09-05 19:27:38 +0800

    [refactor] export directive

--- src/modules/agm-direction.module.ts
+++ src/modules/agm-direction.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { AgmDirection } from '../directive/agm-direction.directive';
 
-// export * from '../directive/agm-direction.directive';
+export * from '../directive/agm-direction.directive';
 
 @NgModule({
     imports: [],
