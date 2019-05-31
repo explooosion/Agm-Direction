7ff0627e44ecc263185971723ef61acd8b56a02f
Robby <ta7382@gmail.com>
2019-05-31 10:14:17 +0800

    [update]  testing angular 8.0.0

--- playground/src/app/app.module.ts
+++ playground/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
@@ -15,7 +14,6 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
     AgmCoreModule.forRoot({
       apiKey: environment.googleAPIKey,
     }),
