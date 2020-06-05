ae26e44d0f13e013f9e16e341e7b15fbb19b2641
explooosion <ta7382@gmail.com>
2020-06-05 22:52:59 +0800

    Adding Support for Angular 9 #93

--- src/modules/agm-direction.module.ts
+++ src/modules/agm-direction.module.ts
@@ -1,21 +1,24 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { AgmDirection } from '../directive/agm-direction.directive';
 
-export * from '../directive/agm-direction.directive';
-
 @NgModule({
-    imports: [],
-    declarations: [
-        AgmDirection,
-    ],
-    exports: [
-        AgmDirection,
-    ]
+  declarations: [
+    AgmDirection,
+  ],
+  exports: [
+    AgmDirection,
+  ]
 })
 export class AgmDirectionModule {
-    static forRoot(): ModuleWithProviders {
-        return {
-            ngModule: AgmDirectionModule,
-        };
-    }
+  public static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: AgmDirectionModule,
+    };
+  }
+
+  public static forChild(): ModuleWithProviders {
+    return {
+      ngModule: AgmDirectionModule,
+    };
+  }
 }
