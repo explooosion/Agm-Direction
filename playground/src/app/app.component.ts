7ff748a4c6f067ca7eec4e5bc08e90c8f50fa95f
explooosion <ta7382@gmail.com>
2020-07-11 13:06:51 +0800

    Add example #96

--- playground/src/app/app.component.ts
+++ playground/src/app/app.component.ts
@@ -3,32 +3,65 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 
   public lat = 24.799448;
   public lng = 120.979021;
 
-  public origin: any = 'No. 1, Section 1, Taiwan Avenue, Central District, Taichung, Taiwan';
-  public destination: any = 'No. 1 Guanqian Road, North District, Taichung City';
+  public origin: any = 'Taichung Railway Station, Taiwan';
+  public destination: any = 'Wenxin Forest Park, Taiwan';
 
-  public renderOptions = {
-    suppressMarkers: true,
-  };
+  public waypoints: Array<any> = [
+    { location: 'Chung Shan Medical University, Taiwan', stopover: true },
+    { location: 'National Chung Hsing University, Taiwan', stopover: true },
+  ];
 
-  public markerOptions = {
-    origin: {
-      icon: 'https://i.imgur.com/7teZKif.png',
-      draggable: true,
-    },
-    destination: {
-      icon: 'https://i.imgur.com/7teZKif.png',
-      draggable: true,
-      infoWindow: `
-        <h4>Hello<h4>
-        <a href='http://www-e.ntust.edu.tw/home.php' target='_blank'>Taiwan Tech</a>
-        `
-    },
-  };
+  // Hide origin polylines
+  public renderOptions = { suppressPolylines: true };
+
+  // Custom polylines
+  public polylines: Array<google.maps.Polyline> = [];
+
+  // Current map
+  public map: google.maps.Map;
+
+  // Save GoogleMap instance
+  public onMapReady(event: google.maps.Map) {
+    this.map = event;
+  }
+
+  public async onResponse(event: google.maps.DirectionsResult) {
+
+    // Default style
+    const polylineOptions: google.maps.PolygonOptions = {
+      strokeWeight: 6,
+      strokeOpacity: 0.55,
+    };
+
+    // Polylines strokeColor
+    const colors = ['#0000FF', '#FF0000', '#0000FF'];
+
+    // Clear exist polylines
+    this.polylines.forEach(polyline => polyline.setMap(null));
+
+    const { legs } = event.routes[0];
+
+    legs.forEach((leg, index) => {
+
+      leg.steps.forEach(step => {
+        const nextSegment = step.path;
+        const stepPolyline = new google.maps.Polyline(polylineOptions);
+
+        // Custom color
+        stepPolyline.setOptions({ strokeColor: colors[index] });
+
+        nextSegment.forEach(next => stepPolyline.getPath().push(next));
+
+        this.polylines.push(stepPolyline);
+        stepPolyline.setMap(this.map);
+      });
+    });
+  }
 }
