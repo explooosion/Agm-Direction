ed994e3e94262cef48eddd2ffdbbd8f67653e2cd
Robby <ta7382@gmail.com>
2021-03-19 02:43:24 +0800

    Update playground with v11

--- playground/src/app/app.component.ts
+++ playground/src/app/app.component.ts
@@ -13,55 +13,23 @@ export class AppComponent {
   public origin: any = 'Taichung Railway Station, Taiwan';
   public destination: any = 'Wenxin Forest Park, Taiwan';
 
-  public waypoints: Array<any> = [
-    { location: 'Chung Shan Medical University, Taiwan', stopover: true },
-    { location: 'National Chung Hsing University, Taiwan', stopover: true },
-  ];
-
-  // Hide origin polylines
-  public renderOptions = { suppressPolylines: true };
-
-  // Custom polylines
-  public polylines: Array<google.maps.Polyline> = [];
-
-  // Current map
-  public map: google.maps.Map;
-
-  // Save GoogleMap instance
-  public onMapReady(event: google.maps.Map): void {
-    this.map = event;
+  public renderOptions = {
+    suppressMarkers: true,
   }
 
-  public onResponse(event: google.maps.DirectionsResult): void {
+  public markerOptions = {
+    origin: {
+      draggable: true,
+    },
+    destination: {
+      draggable: true,
+    },
+  }
 
-    // Default style
-    const polylineOptions: google.maps.PolygonOptions = {
-      strokeWeight: 6,
-      strokeOpacity: 0.55,
-    };
-
-    // Polylines strokeColor
-    const colors = ['#0000FF', '#FF0000', '#0000FF'];
-
-    // Clear exist polylines
-    this.polylines.forEach(polyline => polyline.setMap(null));
-
-    const { legs } = event.routes[0];
-
-    legs.forEach((leg, index) => {
-
-      leg.steps.forEach(step => {
-        const nextSegment = step.path;
-        const stepPolyline = new google.maps.Polyline(polylineOptions);
-
-        // Custom color
-        stepPolyline.setOptions({ strokeColor: colors[index] });
-
-        nextSegment.forEach(next => stepPolyline.getPath().push(next));
-
-        this.polylines.push(stepPolyline);
-        stepPolyline.setMap(this.map);
-      });
-    });
+  public change(event: any) {
+    const start = event.routes[0].legs[0].start_location;
+    const end = event.routes[0].legs[0].end_location;
+    console.log('start', start.lat(), start.lng());
+    console.log('end', end.lat(), end.lng());
   }
 }
